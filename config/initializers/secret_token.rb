b3cd3680df89506ea30cb1314030a43b2c98ec6c
Andreas Argelius <andreas@sanae.(none)>
2014-01-12 21:35:18 +0100

    first

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Lilliputian::Application.config.secret_key_base = 'c71ba686f03fe4e14720497aafa16c56551af66ddcbd342a0cd043babbf6a00b878ff8d194148c353a12c99c74d8348bbccecc52ee5178b8a43747957bea55b3'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Lilliputian::Application.config.session_store :cookie_store, key: '_lilliputian_session'
